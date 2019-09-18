1b05ab6ebb180a03b68113c6766b66dc31a069ed
Luís Leite <ochii@leite.xyz>
2019-09-18 18:28:34 +0100

    Move common decryption classes members to base decryption class. Use gsl::span in Decrypt method

--- headers/ciphers/basecipher.hpp
+++ headers/ciphers/basecipher.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-#include <stdint.h>
+#include <cstdint>
+#include <gsl/gsl>
 #include <string_view>
 
 namespace uc2
@@ -11,7 +12,12 @@ public:
     virtual ~IBaseCipher() = default;
     virtual void Initialize(std::string_view key, std::string_view iv,
                             bool paddingEnabled = false) = 0;
-    virtual uint64_t Decrypt(const void* pStart, void* pOutBuffer,
-                             const uint64_t iSize) = 0;
+    virtual uint64_t Decrypt(gsl::span<const std::uint8_t> inData,
+                             gsl::span<std::uint8_t> outBuffer) = 0;
+
+protected:
+    std::string_view m_szvKey;
+    std::string_view m_szvIV;
+    bool m_bPaddingEnabled;
 };
 }  // namespace uc2
--- headers/ciphers/descipher.hpp
+++ headers/ciphers/descipher.hpp
@@ -12,12 +12,7 @@ public:
 
     virtual void Initialize(std::string_view key, std::string_view iv,
                             bool paddingEnabled = false);
-    virtual uint64_t Decrypt(const void* pStart, void* pOutBuffer,
-                             const uint64_t iSize);
-
-protected:
-    std::string_view m_szvKey;
-    std::string_view m_szvIV;
-    bool m_bPaddingEnabled;
+    virtual uint64_t Decrypt(gsl::span<const std::uint8_t> inData,
+                             gsl::span<std::uint8_t> outBuffer);
 };
 }  // namespace uc2
