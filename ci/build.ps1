96ccd66378537ec961ba4d3e3dc02ff2e421efdb
Luís Leite <ochii@leite.xyz>
2019-09-22 02:06:39 +0100

    CI scripts: setup MSVC tools path in build.ps1

--- ci/build.ps1
+++ ci/build.ps1
@@ -1,32 +1,59 @@
+function SetupVsToolsPath {
+    # from https://allen-mack.blogspot.com/2008/03/replace-visual-studio-command-prompt.html
+
+    # split location to shorten the command
+    Push-Location 'C:\Program Files (x86)\Microsoft Visual Studio\2017'
+    Push-Location '.\Community\VC\Auxiliary\Build'
+
+    cmd /c "vcvars64.bat&set" |
+    ForEach-Object {
+        if ($_ -match "=") {
+            $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+        }
+    }
+
+    Pop-Location
+    Pop-Location
+}
+
 $curBuildCombo = $env:BUILD_COMBO
 $curConfig = $env:CONFIGURATION
 
 Write-Host "Running build script..."
 Write-Host "Current script build combo is: $curBuildCombo $curConfig"
 
-$targetCompilerCC;
-$targetCompilerCXX;
+$targetCompilerCC = '';
+$targetCompilerCXX = '';
 
 switch ($curBuildCombo) {
     "linux-gcc" {
         $targetCompilerCC = 'gcc-9'
         $targetCompilerCXX = 'g++-9'
+        break
     }
     "linux-clang" {
         $targetCompilerCC = 'clang-8'
         $targetCompilerCXX = 'clang++-8'
+        break
     }
     "windows-mingw" {
         $targetCompilerCC = 'C:\\msys64\\mingw64\\bin\\gcc.exe'
         $targetCompilerCXX = 'C:\\msys64\\mingw64\\bin\\g++.exe'
+        break
     }
     "windows-msvc" {
         $targetCompilerCC = 'cl'
         $targetCompilerCXX = 'cl'
+        # add msvc 17 tools to path
+        SetupVsToolsPath
+        break
     }
     "windows-clang" {
-        $targetCompilerCC = 'clang-cl'
-        $targetCompilerCXX = 'clang-cl'
+        $targetCompilerCC = 'clang'
+        $targetCompilerCXX = 'clang'
+        # add msvc 17 tools to path
+        #SetupVsToolsPath
+        break
     }
     Default {
         Write-Error 'Unknown build combo used, could not find appropriate compiler.'
@@ -36,6 +63,9 @@ switch ($curBuildCombo) {
 
 Write-Host "Selected C compiler: $targetCompilerCC"
 Write-Host "Selected C++ compiler: $targetCompilerCXX"
+
+# create build dir
+CreateDirectory ./build
 
 # go to build dir
 Push-Location ./build
--- ci/setup.ps1
+++ ci/setup.ps1
@@ -3,20 +3,6 @@ function CreateDirectory {
     New-Item -ItemType Directory -Path $newDirectory
 }
 
-function SetupVsToolsPath {
-    # from https://allen-mack.blogspot.com/2008/03/replace-visual-studio-command-prompt.html
-    Push-Location 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build'
-
-    cmd /c "vcvars64.bat&set" |
-    ForEach-Object {
-        if ($_ -match "=") {
-            $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
-        }
-    }
-
-    Pop-Location
-}
-
 function PrintToolsVersion {
     param ([string]$curBuildCombo)
 
@@ -29,25 +15,30 @@ function PrintToolsVersion {
             which gcc-9
             Write-Host '# GCC'
             gcc-9 -v
+            break
         }
         "linux-clang" {
             which clang-8
             Write-Host '# Clang - GCC/Linux'
             clang-8 -v
+            break
         }
         "windows-mingw" {
             Write-Host '# MinGW'
             C:\msys64\mingw64\bin\gcc.exe -v
+            break
         }
         "windows-msvc" {
             which cl
             Write-Host '# MSVC'
             cl
+            break
         }
         "windows-clang" {
             which clang-cl
             Write-Host '# Clang - Windows'
             clang-cl -v
+            break
         }
         Default {
             Write-Error 'Unknown build combo used, could not find appropriate compiler.'
@@ -74,8 +65,8 @@ $curBuildCombo = $env:BUILD_COMBO
 #$isGccBuild = $curBuildCombo -eq 'linux-gcc' # unused
 $isLinuxClangBuild = $curBuildCombo -eq 'linux-clang'
 # $isMingwBuild = $curBuildCombo -eq 'windows-mingw' # unused
-$isMsvcBuild = $curBuildCombo -eq 'windows-msvc'
-$isWinClangBuild = $curBuildCombo -eq 'windows-clang'
+#$isMsvcBuild = $curBuildCombo -eq 'windows-msvc'
+#$isWinClangBuild = $curBuildCombo -eq 'windows-clang'
 
 Write-Host "Running setup script..."
 Write-Host "Current setup build combo is: $curBuildCombo"
@@ -97,11 +88,6 @@ elseif ($isWindows) {
 
     # install ninja through scoop
     scoop install ninja
-
-    if ($isMsvcBuild -or $isWinClangBuild) {
-        # add msvc 17 tools to path
-        SetupVsToolsPath
-    }
 }
 else {
     Write-Error 'An unknown OS is running this script, implement me.'
@@ -113,6 +99,3 @@ PrintToolsVersion $curBuildCombo
 
 # setup submodules
 git submodule update --init --recursive
-
-# create build dir
-CreateDirectory ./build
