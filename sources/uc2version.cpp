279773abd8ad6ef1b44e57ff5abc888b8925279a
Luís Leite <ochii@leite.xyz>
2019-09-25 01:38:06 +0100

    Version 1.1.1

--- sources/uc2version.cpp
+++ sources/uc2version.cpp
@@ -8,8 +8,8 @@ namespace uc2
 {
 constexpr const int MAJOR_VERSION = 1;
 constexpr const int MINOR_VERSION = 1;
-constexpr const int PATCH_VERSION = 0;
-constexpr const std::string_view VERSION_STRING = "1.1.0"sv;
+constexpr const int PATCH_VERSION = 1;
+constexpr const std::string_view VERSION_STRING = "1.1.1"sv;
 
 int Version::GetMajorVersionNum()
 {
