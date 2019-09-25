19ce99d2d67f5b86010e3739bb894da03bdf29eb
Luís Leite <ochii@leite.xyz>
2019-09-25 01:37:29 +0100

    CI tests: copy required libraries to the test app's directory

--- ci/test.ps1
+++ ci/test.ps1
@@ -1,12 +1,27 @@
 Write-Host 'Running test script...'
 
+$curBuildCombo = $env:BUILD_COMBO
+
+$isMingwBuild = $curBuildCombo -eq 'windows-mingw'
+$isMsvcBuild = $curBuildCombo -eq 'windows-msvc'
+
 # go to the test binary dir
 Push-Location ./build/tests    
 
 if ($isLinux) {
     ./pkg_test
 }
-elseif ($isWindows) {    
+elseif ($isWindows) {   
+    if ($isMingwBuild) {        
+        # copy libuncso2 and crypto++ dlls to the tests directory
+        Copy-Item "..\libuncso2.dll" -Destination ".\"
+        Copy-Item "..\external\cryptopp\libcryptopp.dll" -Destination ".\"
+    }
+    elseif ($isMsvcBuild) {        
+        # copy libuncso2 dll to the tests directory
+        Copy-Item "..\uncso2.dll" -Destination ".\"
+    }
+
     ./pkg_test.exe
 }
 else {
